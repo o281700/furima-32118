ef68cb38b245213e98e81e36a92e2d77551ed239
o281700 <siba0311@gmail.com>
2020-11-20 20:16:19 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configuer_permitted_parameters, if: :devise_controller?
+
+  private
+  def configuer_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [
+      :nickname, :last_name, :first_name,
+      :last_kana, :first_kana, :birthday ])
+  end
 end
