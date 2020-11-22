87016d5dfd0ca4e6e6d44f1d4702a43a8aee2372
o281700 <siba0311@gmail.com>
2020-11-22 19:21:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
