03cebeb0d9b9c6ba5ad316d574a367cd5ee2d26a
o281700 <siba0311@gmail.com>
2020-11-24 00:10:43 +0900

    出品機能ビュー編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,24 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :fee
   belongs_to_active_hash :area
-  belongs_to_active_hash :days
+  belongs_to_active_hash :day
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :explain
     validates :category_id, numericality: { other_than: 1 } 
-    validates :statusu_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 } 
     validates :fee_id, numericality: { other_than: 1 } 
     validates :area_id, numericality: { other_than: 1 } 
     validates :days_id, numericality: { other_than: 1 } 
     validates :price
-    validates :user_id, foreign_key: true
+    validates :user_id
   end
 end
