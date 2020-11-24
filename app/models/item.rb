574426a3a5a8c64bbaf10fb3ccfea41dfecebc9d
o281700 <siba0311@gmail.com>
2020-11-24 17:11:23 +0900

    エラー処理のためバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,12 +13,15 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :explain
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :status_id, numericality: { other_than: 1 } 
-    validates :fee_id, numericality: { other_than: 1 } 
-    validates :area_id, numericality: { other_than: 1 } 
-    validates :days_id, numericality: { other_than: 1 } 
-    validates :price
+    with_options numericality: { other_than: 1,  message: 'status Select'} do
+      validates :category_id
+      validates :status_id
+      validates :fee_id
+      validates :area_id
+      validates :days_id
+    end
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
     validates :user_id
   end
 end
