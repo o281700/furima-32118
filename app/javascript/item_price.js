970423cb651cf64a66df769d048b09ca49df7397
o281700 <siba0311@gmail.com>
2020-11-24 14:30:39 +0900

    販売手数料/利益計算の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+function itemPrice (){
+  const inputPrice = document.getElementById("item-price");
+  inputPrice.addEventListener('input', () => {
+    const taxPrice = parseInt(inputPrice.value * 0.1);
+    const taxDisplay = document.getElementById("add-tax-price");
+    const profitDisplay = document.getElementById("profit");
+    taxDisplay.innerHTML = `${taxPrice}`;
+    profitDisplay.innerHTML = `${inputPrice.value - taxPrice}`
+  });
+};
+window.addEventListener('load', itemPrice);
