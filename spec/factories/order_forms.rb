3353620ed205b6f7546694708f3660cd21641266
o281700 <siba0311@gmail.com>
2020-11-29 00:04:23 +0900

    配送先テーブル保存のテストコード

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_form do
+    postal { '123-4567' }
+    area_id { '15' }
+    city { '横浜区緑区' }
+    add { '青山1-1-1' }
+    bldg { '柳ビル103' }
+    tel { '09012345678' }
+  end
+end
---
+++ spec/models/order_form_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe OrderForm, type: :model do
+  before do
+    @order_form = FactoryBot.build(:order_form)
+  end
+
+  describe '商品購入機能' do
+    context '購入者情報が保存できるとき' do
+      it '全ての情報が入力されていれば保存できる' do
+        expect(@order_form).to be_valid
+      end
+    end
+    context '購入者情報が保存できないとき' do
+      it '郵便番号が空白のとき' do
+        @order_form.postal = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal can't be blank")
+      end
+      it '郵便番号に"-"が入っていないとき' do
+        @order_form.postal = '1234567'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Postal Input correctly')
+      end
+      it '都道府県を選択していないとき' do
+        @order_form.area_id = '1'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Area Select')
+      end
+      it '市区町村が空白のとき' do
+        @order_form.city = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空白のとき' do
+        @order_form.add = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Add can't be blank")
+      end
+      it '電話番号が空白のとき' do
+        @order_form.tel = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Tel can't be blank")
+      end
+      it '電話番号に"-"が入っているとき' do
+        @order_form.tel = '090-1234567'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Tel Input only number')
+      end
+    end
+  end
+end
