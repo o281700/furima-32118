80c62185c61b5430c1effd4bb2580a7c5d004d54
o281700 <siba0311@gmail.com>
2020-12-30 17:11:29 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items do
     resources :orders, only: [:index, :create]
+    resources :comments, only: [:create]
   end
 end
