19aae1bb8d15d8760adfc359f05be9108b860108
o281700 <siba0311@gmail.com>
2020-12-31 02:47:05 +0900

    showページ作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -2,4 +2,9 @@ class CategorysController < ApplicationController
   def index
     @categorys = Category.where(id: 2..11)
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_items = Item.where(category_id: params[:id])
+  end
 end
