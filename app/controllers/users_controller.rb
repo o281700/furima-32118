fb558d0db4fc44d60833bd04564d072390fa94f0
o281700 <siba0311@gmail.com>
2021-01-03 00:31:54 +0900

    ルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit]
 
   def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
